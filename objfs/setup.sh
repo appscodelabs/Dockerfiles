fb626b47f1e675e525ef1f840b2abcffadccce82
Tamal Saha <tamal@appscode.com>
2017-05-08 23:39:19 -0700

    Use debian 8.8 (#5)

--- objfs/setup.sh
+++ objfs/setup.sh
@@ -9,7 +9,7 @@ source "$LIB_ROOT/hack/libbuild/common/lib.sh"
 source "$LIB_ROOT/hack/libbuild/common/public_image.sh"
 
 IMG=${PWD##*/}
-TAG=8.7
+TAG=8.8
 PRIVILEGED_CONTAINER='--privileged=true'
 # CAP_SYS_ADMIN was not enough. I got errors like, can't access /dev/fuse
 
