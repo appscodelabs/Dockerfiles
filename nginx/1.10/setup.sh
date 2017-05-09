3791af549bf5c9435fd443bbf4b6104313f3780b
Tamal Saha <tamal@appscode.com>
2017-05-09 00:04:32 -0700

    Move nginx 1.10 to its own folder (#6)

--- nginx/1.10/setup.sh
+++ nginx/1.10/setup.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-LIB_ROOT=$(dirname "${BASH_SOURCE}")/..
+LIB_ROOT=$(dirname "${BASH_SOURCE}")/../..
 source "$LIB_ROOT/hack/libbuild/common/lib.sh"
 source "$LIB_ROOT/hack/libbuild/common/public_image.sh"
 
