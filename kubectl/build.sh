8c0b798a6fd54b35581b24d72938ccb8771b0533
Tamal Saha <tamal@appscode.com>
2018-09-03 15:58:14 -0700

    Update kubectl dockerfiles

---
+++ kubectl/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -euo pipefail
+
+for ver in 1.7 1.8 1.9 1.10 1.11
+do
+	latest=$(curl -fsSL https://storage.googleapis.com/kubernetes-release/release/stable-$ver.txt)
+
+	wget https://dl.k8s.io/$latest/kubernetes-client-linux-amd64.tar.gz \
+	  && tar -xzvf kubernetes-client-linux-amd64.tar.gz
+
+	docker build -t appscode/kubectl:$latest .
+	docker push appscode/kubectl:$latest
+
+	docker tag appscode/kubectl:$latest appscode/kubectl:v$ver
+	docker push appscode/kubectl:v$ver
+
+	docker tag appscode/kubectl:$latest appscode/kubectl:$ver
+	docker push appscode/kubectl:$ver
+
+	rm -rf kubernetes kubernetes-client-linux-amd64.tar.gz
+done
