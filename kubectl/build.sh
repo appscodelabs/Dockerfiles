28504ec88bfb4dc36ce3d5ee42d293e705c42279
Tamal Saha <tamal@appscode.com>
2020-06-01 17:22:25 -0700

    Update build.sh
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- kubectl/build.sh
+++ kubectl/build.sh
@@ -2,7 +2,7 @@
 set -x
 set -euo pipefail
 
-for ver in 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16
+for ver in 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18
 do
 	latest=$(curl -fsSL https://storage.googleapis.com/kubernetes-release/release/stable-$ver.txt)
 
